243388c7e056db0732e5406dac95c39071f4286a
Swei Chen <weiweipooh0615@gmail.com>
2019-05-10 14:25:24 +0800

    intergration

---
+++ intergration.py
@@ -0,0 +1,18 @@
+def anonymous(x):
+	return x**2 + 1
+
+def integrate(fun, start, end):
+	step = 0.1
+	intercept = start
+	area = 0
+	while intercept < end:
+		intercept += step
+		high = fun(intercept)
+		
+		area = area + step*high
+		
+	''' your work here '''
+	return area
+
+
+print(integrate(anonymous, 0, 10))
